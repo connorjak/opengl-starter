89aa8294eef0a9ea712b24330e91acae9b4374f6
Connor Jakubik <connor.jakubik@gmail.com>
2022-09-22 02:32:12 -0500

    Separate glsl, shader class

--- opengl_starter/CSCE649_HW2.cpp
+++ opengl_starter/CSCE649_HW2.cpp
@@ -5,7 +5,9 @@
 /* You can just copy the headers from glm, or go through the install */
 //#include<glad/glad.h>
 //#include<GLFW/glfw3.h>
-#include<iostream>
+#include <iostream>
+#include <fstream>
+#include <sstream>
 //#include <glm/glm.hpp>
 //#include <glm/gtc/matrix_transform.hpp>
 //#include <glm/gtc/type_ptr.hpp>
@@ -52,19 +54,36 @@ const char* vertexShaderSource = "#version 330 core\n"
 "uniform mat4 model;\n"
 "uniform mat4 view;\n"
 "uniform mat4 projection;\n"
-"out vec3 ourColor;\n"
+"out vec3 vertColor;\n"
+"out vec3 normal;\n"
 "void main()\n"
 "{\n"
 "   gl_Position = projection * view * model * vec4(aPos, 1.0);\n"
-"   ourColor = aColor;\n"
+"   vertColor = aColor;\n"
+"   normal = aPos.normalize();\n"
 "}\0";
+
+static int vtxAttributeIdx_Position = 0;
+static int vtxAttributeIdx_Color = 1;
+
 const char* fragmentShaderSource = "#version 330 core\n"
 "out vec4 FragColor;\n"
-"in vec3 ourColor;\n"
+"in vec3 vertColor;\n"
+"in vec3 normal;\n"
 "void main()\n"
 "{\n"
-"   FragColor = vec4(ourColor, 1.0f);\n"
+"   FragColor = vec4(vertColor, 1.0f);\n"
 "}\n\0";
+
+const char* planetFragmentShaderSource = "#version 330 core\n"
+"uniform vec3 lightDir;\n"
+"out vec4 FragColor;\n"
+"in vec3 vertColor;\n"
+"void main()\n"
+"{\n"
+"   FragColor = vec4(vertColor, 1.0f);\n"
+"}\n\0";
+
 // View parameters
 float theta = 0.0;
 float phi = 0.0;
@@ -186,6 +205,36 @@ float ball[] = {
      br,  0,  0,   1.0f, 1.0f, 1.0f,
       0,  0,-br,   1.0f, 1.0f, 1.0f,
 };
+
+float pr = 0.3;
+float planet[] = {
+    // positions         // colors
+     pr,  0,  0,   1.0f, 1.0f, 1.0f, // triangle 1
+      0, pr,  0,   1.0f, 1.0f, 1.0f,
+      0,  0, pr,   1.0f, 1.0f, 1.0f,
+      0, pr,  0,   1.0f, 1.0f, 1.0f, // triangle 2
+    -pr,  0,  0,   1.0f, 1.0f, 1.0f,
+      0,  0, pr,   1.0f, 1.0f, 1.0f,
+    -pr,  0,  0,   1.0f, 1.0f, 1.0f, // triangle 3
+      0,-pr,  0,   1.0f, 1.0f, 1.0f,
+      0,  0, pr,   1.0f, 1.0f, 1.0f,
+      0,-pr,  0,   1.0f, 1.0f, 1.0f, // triangle 4
+     pr,  0,  0,   1.0f, 1.0f, 1.0f,
+      0,  0, pr,   1.0f, 1.0f, 1.0f,
+     pr,  0,  0,   1.0f, 1.0f, 1.0f, // triangle 5
+      0,-pr,  0,   1.0f, 1.0f, 1.0f,
+      0,  0,-pr,   1.0f, 1.0f, 1.0f,
+      0,-pr,  0,   1.0f, 1.0f, 1.0f, // triangle 6
+    -pr,  0,  0,   1.0f, 1.0f, 1.0f,
+      0,  0,-pr,   1.0f, 1.0f, 1.0f,
+    -pr,  0,  0,   1.0f, 1.0f, 1.0f, // triangle 7
+      0, pr,  0,   1.0f, 1.0f, 1.0f,
+      0,  0,-pr,   1.0f, 1.0f, 1.0f,
+      0, pr,  0,   1.0f, 1.0f, 1.0f, // triangle 8
+     pr,  0,  0,   1.0f, 1.0f, 1.0f,
+      0,  0,-pr,   1.0f, 1.0f, 1.0f,
+};
+
 
 
 bool randomBool()
@@ -358,7 +407,148 @@ public:
 };
 
 
+using buffer_id_t = unsigned int;
+using shader_id_t = unsigned int;
+using program_id_t = unsigned int;
+using matrix_id_t = unsigned int;
 
+
+static void Better_glVertexAttribPointer(int attributeToConfigureIdx, int numElementsPerVertex,
+    int gl_datatype, bool normalized, size_t strideBytesPerVertex, void* offset)
+{
+    glVertexAttribPointer(attributeToConfigureIdx, numElementsPerVertex,
+        gl_datatype, normalized, strideBytesPerVertex, offset);
+}
+
+static void Better_glEnableVertexAttribArray(int attributeToConfigureIdx)
+{
+    glEnableVertexAttribArray(attributeToConfigureIdx);
+}
+
+static void Better_glUniformMatrix4fv(matrix_id_t mat, int howManyMatricesToSend, bool transpose, glm::f32* value)
+{
+    glUniformMatrix4fv(mat, howManyMatricesToSend, transpose, value);
+}
+
+
+class Shader
+{
+public:
+    // the program ID
+    shader_id_t ID = 0;
+
+    Shader(const char* vertexPath, const char* fragmentPath)
+    {
+
+        // 1. retrieve the vertex/fragment source code from filePath
+        std::string vertexCode;
+        std::string fragmentCode;
+        std::ifstream vShaderFile;
+        std::ifstream fShaderFile;
+        // ensure ifstream objects can throw exceptions:
+        vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+        fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+        try
+        {
+            // open files
+            vShaderFile.open(vertexPath);
+            fShaderFile.open(fragmentPath);
+            std::stringstream vShaderStream, fShaderStream;
+            // read file's buffer contents into streams
+            vShaderStream << vShaderFile.rdbuf();
+            fShaderStream << fShaderFile.rdbuf();
+            // close file handlers
+            vShaderFile.close();
+            fShaderFile.close();
+            // convert stream into string
+            vertexCode = vShaderStream.str();
+            fragmentCode = fShaderStream.str();
+        }
+        catch (std::ifstream::failure e)
+        {
+            std::cout << "ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ" << std::endl;
+        }
+        const char* vShaderCode = vertexCode.c_str();
+        const char* fShaderCode = fragmentCode.c_str();
+
+        // 2. compile shaders
+        unsigned int vertex, fragment;
+        int success;
+        char infoLog[512];
+
+        // vertex Shader
+        vertex = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vertex, 1, &vShaderCode, NULL);
+        glCompileShader(vertex);
+        // print compile errors if any
+        glGetShaderiv(vertex, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(vertex, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n"
+                      << infoLog << std::endl;
+        };
+
+        // similiar for Fragment Shader
+        fragment = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(fragment, 1, &fShaderCode, NULL);
+        glCompileShader(fragment);
+        // print compile errors if any
+        glGetShaderiv(fragment, GL_COMPILE_STATUS, &success);
+        if (!success)
+        {
+            glGetShaderInfoLog(fragment, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n"
+                      << infoLog << std::endl;
+        };
+
+        // shader Program
+        ID = glCreateProgram();
+        glAttachShader(ID, vertex);
+        glAttachShader(ID, fragment);
+        glLinkProgram(ID);
+        // print linking errors if any
+        glGetProgramiv(ID, GL_LINK_STATUS, &success);
+        if (!success)
+        {
+            glGetProgramInfoLog(ID, 512, NULL, infoLog);
+            std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n"
+                      << infoLog << std::endl;
+        }
+
+        // delete the shaders as they're linked into our program now and no longer necessary
+        glDeleteShader(vertex);
+        glDeleteShader(fragment);
+    }
+
+    ~Shader()
+    {
+        glDeleteProgram(ID);
+    }
+
+    // use/activate the shader
+    void use()
+    {
+        glUseProgram(ID);
+    }
+    // utility uniform functions
+    void setBool(const std::string& name, bool value) const
+    {
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+    }
+    void setInt(const std::string& name, int value) const
+    {
+        glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+    }
+    void setFloat(const std::string& name, float value) const
+    {
+        glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+    }
+    void setMat4(const std::string& name, glm::mat4 value) const
+    {
+        Better_glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, false, glm::value_ptr(value));
+    }
+};
 
 
 
@@ -413,24 +603,14 @@ int main(int argc, char* argv[])
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_CULL_FACE);
     glCullFace(GL_BACK);
-    // vertex shader
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    // fragment shader
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    // link shaders
-    unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-    glUseProgram(shaderProgram);
+
+    Shader defaultShader("../../../../shaders/default.vs", "../../../../shaders/default.fs");
+    Shader planetShader("../../../../shaders/default.vs", "../../../../shaders/default.fs");
+
+    
+    defaultShader.use();
     // Set up vertex array object (VAO) and vertex buffers for box and ball
-    unsigned int boxbuffer, ballbuffer, VAO;
+    buffer_id_t boxbuffer, ballbuffer, VAO;
     glGenVertexArrays(1, &VAO);
     glBindVertexArray(VAO);
     glGenBuffers(1, &boxbuffer);
@@ -439,17 +619,39 @@ int main(int argc, char* argv[])
     glGenBuffers(1, &ballbuffer);
     glBindBuffer(GL_ARRAY_BUFFER, ballbuffer);
     glBufferData(GL_ARRAY_BUFFER, sizeof(ball), ball, GL_STATIC_DRAW);
-    // Declare model/view/projection matrices
+    // position attribute 
+    Better_glVertexAttribPointer(vtxAttributeIdx_Position, 3, GL_FLOAT, false, 6 * sizeof(float), (void*)0);
+    Better_glEnableVertexAttribArray(vtxAttributeIdx_Position);
+    // color attribute
+    Better_glVertexAttribPointer(vtxAttributeIdx_Color, 3, GL_FLOAT, false, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    Better_glEnableVertexAttribArray(vtxAttributeIdx_Color);
+
+    //planetShader.use();
+    // Set up vertex array object (VAO) and vertex buffers for planet
+    buffer_id_t planetbuffer, VAO_planet;
+    glGenVertexArrays(1, &VAO_planet);
+    glBindVertexArray(VAO_planet);
+    glGenBuffers(1, &planetbuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, planetbuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(planet), planet, GL_STATIC_DRAW);
+    // position attribute
+    Better_glVertexAttribPointer(vtxAttributeIdx_Position, 3, GL_FLOAT, false, 6 * sizeof(float), (void*)0);
+    Better_glEnableVertexAttribArray(vtxAttributeIdx_Position);
+    // color attribute
+    Better_glVertexAttribPointer(vtxAttributeIdx_Color, 3, GL_FLOAT, false, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    Better_glEnableVertexAttribArray(vtxAttributeIdx_Color);
+
+    // Declare model/view/projection matrices, which are used as inputs in the vertex shader
     glm::mat4 model = glm::mat4(1.0f);
-    unsigned int modelLoc = glGetUniformLocation(shaderProgram, "model");
+    //matrix_id_t modelLoc = glGetUniformLocation(shaderProgram, "model");
     glm::mat4 view = glm::mat4(1.0f);
-    unsigned int viewLoc = glGetUniformLocation(shaderProgram, "view");
+    //matrix_id_t viewLoc = glGetUniformLocation(shaderProgram, "view");
     glm::mat4 projection = glm::mat4(1.0f);
-    unsigned int projectionLoc = glGetUniformLocation(shaderProgram, "projection");
+    //matrix_id_t projectionLoc = glGetUniformLocation(shaderProgram, "projection");
     // Set Projection matrix
     projection = glm::perspective(glm::radians(45.0f), (float)SCR_WIDTH / (float)SCR_HEIGHT, 0.1f, 100.0f);
-    glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
-    /* HERE IS INITIAL BALL POSITION, SET AT 0,0,0 FOR DEMO */
+    defaultShader.setMat4("projection", projection);
+    planetShader.setMat4("projection", projection);
 
 
     ///////////////////////////////////////////
@@ -485,40 +687,31 @@ int main(int argc, char* argv[])
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         // Set view matrix
         view = glm::lookAt(glm::vec3(camX, camY, camZ), glm::vec3(0.0, 0.0, 0.0), glm::vec3(0.0, 0.0, 1.0));
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
- 
-        //// render the box
-        //glBindBuffer(GL_ARRAY_BUFFER, boxbuffer);
-        //// position attribute
-        //glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-        //glEnableVertexAttribArray(0);
-        //// color attribute
-        //glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-        //glEnableVertexAttribArray(1);
-        //// draw the box (no model transform needed)
-        //model = glm::mat4(1.0f);
-        //glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-        //glDrawArrays(GL_TRIANGLES, 0, 36);
+        defaultShader.setMat4("view", view);
+        planetShader.setMat4("view", view);
 
-        // render the ball
-        glBindBuffer(GL_ARRAY_BUFFER, ballbuffer);
-        // position attribute 
-        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
-        glEnableVertexAttribArray(0);
+        //Wireframe/not
+        //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        //glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
 
+        //// render planets
+        //planetShader.use();
+        //glBindVertexArray(VAO_planet);
+
+
+        // render balls 
+        defaultShader.use();
+        glBindVertexArray(VAO);
 
         for (auto ball : balls)
         {
-            float ballcolor[3] = { 255,255,255 };
-            // color attribute
-            glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
-            glEnableVertexAttribArray(1);
             // Translate ball to its position and draw
             model = glm::mat4(1.0f);
             model = glm::translate(model, glm::vec3(ball->getpos(0), ball->getpos(1), ball->getpos(2)));
-            glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
+            defaultShader.setMat4("model", model);
             glDrawArrays(GL_TRIANGLES, 0, 24);
         }
+
             
 
 
@@ -563,11 +756,12 @@ int main(int argc, char* argv[])
         }*/
                 
     }
+
     // optional: de-allocate all resources once they've outlived their purpose:
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &boxbuffer);
     glDeleteBuffers(1, &ballbuffer);
-    glDeleteProgram(shaderProgram);
-glfwTerminate();
-return 0;
+    //glDeleteProgram(shaderProgram);
+    glfwTerminate();
+    return 0;
 }
